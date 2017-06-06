58cfb83b8d43f1ebd76659f0e63706878d819e1d
bcarey <bcarey@LAPTOP-9IJC4N3L>
2017-06-06 15:45:22 -0400

    formatting changes

--- src/main/java/com/careydevelopment/ajaxdemo/controller/AjaxDemoController.java
+++ src/main/java/com/careydevelopment/ajaxdemo/controller/AjaxDemoController.java
@@ -7,18 +7,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class AjaxDemoController {
 
-	//starting page for AJAX demo
-	@RequestMapping("/ajaxDemo")
-	public String ajaxDemo(Model model) {		
-		//get out
-		return "ajaxDemo";
-	}
-
-	
-	//redirect to demo if user hits the root
-	@RequestMapping("/")
-	public String home(Model model) {
-		return "redirect:ajaxDemo";
-	}
+    //starting page for AJAX demo
+    @RequestMapping("/ajaxDemo")
+    public String ajaxDemo(Model model) {		
+        return "ajaxDemo";
+    }
+    
+    
+    //redirect to demo if user hits the root
+    @RequestMapping("/")
+    public String home(Model model) {
+        return "redirect:ajaxDemo";
+    }
 
 }
